0d43ca3c93f218f7a62ba22ceb5ffd87283b6757
kiddkk84 <kaiye.chen@yahoo.com>
2020-02-02 23:07:32 -0800

    heroku

---
+++ map.js
@@ -0,0 +1,232 @@
+// import * from './mapStyle'
+
+var map; 
+
+function initMap() {
+    const aus = {
+        zoom: 4.25,
+        center: { lat: - 24.874737, lng: 133.823867 },
+        styles: [
+            {
+                "featureType": "all",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "weight": "2.00"
+                    }
+                ]
+            },
+            {
+                "featureType": "all",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "color": "#9c9c9c"
+                    }
+                ]
+            },
+            {
+                "featureType": "all",
+                "elementType": "labels.text",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "color": "#f2f2f2"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#ffffff"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape.man_made",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#ffffff"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "saturation": -100
+                    },
+                    {
+                        "lightness": 45
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#eeeeee"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#7b7b7b"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                    {
+                        "color": "#ffffff"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "simplified"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "labels.icon",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "color": "#46bcec"
+                    },
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#c8d7d4"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#070707"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                    {
+                        "color": "#ffffff"
+                    }
+                ]
+            }
+        ]
+
+
+    }
+        map = new google.maps.Map(
+            document.getElementById('map'), aus);
+
+
+
+
+
+
+    for (let i = 0; i < datas.length; i++) {
+        let lat = datas[i].latitude;
+        let lng = datas[i].longitude;
+        let date = datas[i].acq_date
+        let frp = datas[i].frp; 
+        // console.log(date);
+
+            if (date.includes("2020-01-01")) {
+                dropMarker(lat, lng, frp);
+        } 
+    } 
+
+
+
+}
+
+
+function dropMarker(lat,lng,frp){
+    let image = {
+        url: './fire.png',
+        scaledSize: new google.maps.Size(25, 25),
+    };
+
+    let coord = { lat: lat, lng:lng};
+    var infowindow = new google.maps.InfoWindow({
+        content: 'fire radiative power ' + frp
+    });
+
+    let marker = new google.maps.Marker({
+        position: coord, 
+        map: map,
+        icon: image,
+        })
+
+
+    marker.addListener('mouseover', function () {
+        infowindow.open(map, marker);
+    });
+
+    marker.addListener('mouseout', function () {
+        infowindow.close();
+    });
+}
